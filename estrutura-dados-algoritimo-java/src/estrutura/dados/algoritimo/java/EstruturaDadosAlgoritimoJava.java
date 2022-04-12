9c8da60b641d3a299d47598b1c2c8bc5423130ae
Gledson Emannuel Franco maroja <100153258+GledsonEmannuel@users.noreply.github.com>
2022-04-11 22:36:36 -0300

    Add files via upload

---
+++ estrutura-dados-algoritimo-java/src/estrutura/dados/algoritimo/java/EstruturaDadosAlgoritimoJava.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package estrutura.dados.algoritimo.java;
+
+/**
+ *
+ * @author user
+ */
+public class EstruturaDadosAlgoritimoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
